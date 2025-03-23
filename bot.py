24324dd1940fcacb69a2c74d6347a57fda626bc5
cybardev <50134239+cybardev@users.noreply.github.com>
2025-03-22 22:25:32 -0300

    add user install option

--- bot.py
+++ bot.py
@@ -9,7 +9,13 @@ bot = discord.Bot()
 # ----------------- TODO: MAKE CHANGES BELOW ----------------- #
 
 
-@bot.slash_command(description="Say hello")
+@bot.slash_command(
+    description="Say hello",
+    integration_types={
+        discord.IntegrationType.guild_install,
+        discord.IntegrationType.user_install,
+    },
+)
 @discord.option("name", description="Who to greet")
 async def hello(ctx, name: str = None):
     name = name or ctx.author.name
